f8e487b5d4e1327d650e61390561b94df10fd1f9
AhmadRzKhalili <ahmadrkl2000@gmail.com>
2023-06-29 11:37:03 +0330

    Function "calculate_unigram()" update2

--- main.py
+++ main.py
@@ -10,7 +10,7 @@ def read_corpus(corpus_name):
     return txt
 
 def calculate_unigram(txt):
-    words = []
+    words_list = []
     P_w_list = []
 
     split_words = re.split('\s|\n', txt)
@@ -18,15 +18,15 @@ def calculate_unigram(txt):
     count_total = len(split_words)
     
     for word in split_words:
-        if words.count(word) == 0:
+        if words_list.count(word) == 0:
 
             count_w = split_words.count(word)
             p_w = count_w / count_total
 
-            words.append(word)
+            words_list.append(word)
             P_w_list.append(p_w)
 
-    return {"Word": words, "P_w": P_w_list}
+    return {"Word": words_list, "P_w": P_w_list}
 
 def main():
     unigrams = calculate_unigram(read_corpus("corpus.en"))
