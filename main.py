982d06e903b59b4681eaec8c9d1b2582be208522
AhmadRzKhalili <ahmadrkl2000@gmail.com>
2023-06-29 10:41:15 +0330

    Unigram calculation implemented

--- main.py
+++ main.py
@@ -1,8 +1,32 @@
+def read_corpus(corpus_name):
+    f = open(corpus_name, "r")
+    txt = f.read()
 
+    return txt
+
+def calculate_unigram(txt):
+    words = []
+    unigrams = []
+
+    import re
+    split_words = re.split('\s|\n', txt)
+
+    count_total = len(split_words)
+    
+    for word in split_words:
+        if words.count(word) == 0:
+
+            count_w = split_words.count(word)
+            p_w = count_w / count_total
+            unigrams.append({word: p_w})
+            
+            words.append(word)
+
+    return unigrams
 
 def main():
-    f = open("ak-tune-200.en", "r")
-    print(f.read())
+    print(calculate_unigram(read_corpus("corpus.en")))
+    
 
 if __name__=="__main__":
 	main()
